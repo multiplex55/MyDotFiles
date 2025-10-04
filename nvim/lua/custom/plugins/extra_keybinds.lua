91971eafc29659cc7d8b1e6f56703e5979f923b8
multiplex55 <multiplex55@yahoo.com>
2025-10-04 13:06:28 -0400

    update keybinds

--- nvim/lua/custom/plugins/extra_keybinds.lua
+++ nvim/lua/custom/plugins/extra_keybinds.lua
@@ -261,44 +261,10 @@ return (function()
     return require 'winshift'
   end
 
-  -- Move between windows (Smart Splits aware)
-  vim.keymap.set('n', '<leader>wh', function()
-    smart_splits().move_cursor_left()
-  end, { desc = '[w]indows Left (Smart Splits)' })
-  vim.keymap.set('n', '<leader>wj', function()
-    smart_splits().move_cursor_down()
-  end, { desc = '[w]indows Down (Smart Splits)' })
-  vim.keymap.set('n', '<leader>wk', function()
-    smart_splits().move_cursor_up()
-  end, { desc = '[w]indows Up (Smart Splits)' })
-  vim.keymap.set('n', '<leader>wl', function()
-    smart_splits().move_cursor_right()
-  end, { desc = '[w]indows Right (Smart Splits)' })
-  -- Resize splits with Smart Splits helpers
-  vim.keymap.set('n', '<leader>wH', function()
-    smart_splits().resize_left()
-  end, { desc = '[w]indows Resize ← (Smart Splits)' })
-  vim.keymap.set('n', '<leader>wJ', function()
-    smart_splits().resize_down()
-  end, { desc = '[w]indows Resize ↓ (Smart Splits)' })
-  vim.keymap.set('n', '<leader>wK', function()
-    smart_splits().resize_up()
-  end, { desc = '[w]indows Resize ↑ (Smart Splits)' })
-  vim.keymap.set('n', '<leader>wL', function()
-    smart_splits().resize_right()
-  end, { desc = '[w]indows Resize → (Smart Splits)' })
-  -- Keep arrow-key fallbacks for muscle memory
-  vim.keymap.set('n', '<leader>w<Left>', '<C-w><', { desc = '[w]indows Resize ← (Vim fallback)' })
-  vim.keymap.set('n', '<leader>w<Right>', '<C-w>>', { desc = '[w]indows Resize → (Vim fallback)' })
-  vim.keymap.set('n', '<leader>w<Up>', '<C-w>+', { desc = '[w]indows Resize ↑ (Vim fallback)' })
-  vim.keymap.set('n', '<leader>w<Down>', '<C-w>-', { desc = '[w]indows Resize ↓ (Vim fallback)' })
   -- WinShift integration for advanced rearranging
   vim.keymap.set('n', '<leader>wm', function()
     winshift().cmd_winshift()
   end, { desc = '[w]indows WinShift [m]ove mode (q/Esc to exit)' })
-  vim.keymap.set('n', '<leader>wS', function()
-    winshift().cmd_winshift 'swap'
-  end, { desc = '[w]indows WinShift [S]wap windows' })
   -- Split window
   vim.keymap.set('n', '<leader>wv', '<cmd>vsplit<cr>', { desc = '[w]indows [V]ertical Split' })
   vim.keymap.set('n', '<leader>wb', '<cmd>split<cr>', { desc = '[w]indows Horizontal Split' })
@@ -315,6 +281,9 @@ return (function()
       vim.api.nvim_set_current_win(picked)
     end
   end, { desc = '[w]indow [p]icker' })
+  vim.keymap.set('n', '<leader>wP', function()
+    winshift().cmd_winshift 'swap'
+  end, { desc = '[w]indows WinShift [P]ick other window' })
   --barbar tab navigatio- Buffer navigation
   vim.keymap.set('n', '<Tab>', '<cmd>BufferNext<cr>', { desc = '[t]ab Next' })
   vim.keymap.set('n', '<S-Tab>', '<cmd>BufferPrevious<cr>', { desc = '[t]ab Previous' })
@@ -618,7 +587,7 @@ return (function()
     if not exe or exe == '' then
       return nil
     end
-    return exe:match('^(.*)[/\\][^/\\]-$')
+    return exe:match '^(.*)[/\\][^/\\]-$'
   end
 
   local function window_spy_path(p)
