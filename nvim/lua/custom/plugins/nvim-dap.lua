94cd1ed905711a1f34e8218d4a3bc46b2df88de1
multiplex55 <6619098+multiplex55@users.noreply.github.com>
2025-10-04 18:18:36 -0400

    refactor: lazy-load dap configuration

--- nvim/lua/custom/plugins/nvim-dap.lua
+++ nvim/lua/custom/plugins/nvim-dap.lua
@@ -1,4 +1,16 @@
-return {{
+return {
+  {
     'mfussenegger/nvim-dap',
-    lazy = true
-}}
+    lazy = true,
+    dependencies = {
+      'rcarriga/nvim-dap-ui',
+      'theHamsta/nvim-dap-virtual-text',
+      'nvim-telescope/telescope.nvim',
+      'nvim-telescope/telescope-dap.nvim',
+      'nvim-neotest/nvim-nio',
+    },
+    config = function()
+      require 'custom.dap-config'
+    end,
+  },
+}
