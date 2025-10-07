0f899acf996d93a24212e9f028a86f3e93bfee04
multiplex55 <6619098+multiplex55@users.noreply.github.com>
2025-10-07 18:47:58 -0400

    feat: show macro status in lualine

--- nvim/lua/custom/plugins/lualine.lua
+++ nvim/lua/custom/plugins/lualine.lua
@@ -4,6 +4,30 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
+      local macro_status = function()
+        local recording = vim.fn.reg_recording()
+        if recording ~= '' then
+          return string.format('󰑋 REC @%s', recording)
+        end
+
+        local executing = vim.fn.reg_executing()
+        if executing ~= '' then
+          return string.format('󰑋 RUN @%s', executing)
+        end
+
+        return ''
+      end
+
+      local function refresh_lualine()
+        vim.schedule(function()
+          require('lualine').refresh()
+        end)
+      end
+
+      vim.api.nvim_create_autocmd({ 'RecordingEnter', 'RecordingLeave', 'CmdlineEnter', 'CmdlineLeave' }, {
+        callback = refresh_lualine,
+      })
+
       require('lualine').setup {
         options = {
           theme = 'auto', -- auto picks up current colorscheme
@@ -18,6 +42,14 @@ return {
               'terminal',
             },
           },
+        },
+        sections = {
+          lualine_a = { 'mode' },
+          lualine_b = { 'branch', 'diff', 'diagnostics' },
+          lualine_c = { 'filename', macro_status },
+          lualine_x = { 'encoding', 'fileformat', 'filetype' },
+          lualine_y = { 'progress' },
+          lualine_z = { 'location' },
         },
         winbar = nil,
         inactive_winbar = nil,
