36db6a55bdf116867e09b90be6869275719bf6c4
multiplex55 <6619098+multiplex55@users.noreply.github.com>
2025-10-19 14:01:28 -0400

    Refactor diagnostic highlight setup

--- nvim/lua/custom/highlights.lua
+++ nvim/lua/custom/highlights.lua
@@ -1,4 +1,24 @@
-vim.api.nvim_set_hl(0, 'DiagnosticVirtualTextError', { fg = '#FF5370' })
-vim.api.nvim_set_hl(0, 'DiagnosticVirtualTextWarn', { fg = '#FFCB6B' })
-vim.api.nvim_set_hl(0, 'DiagnosticVirtualTextInfo', { fg = '#82AAFF' })
-vim.api.nvim_set_hl(0, 'DiagnosticVirtualTextHint', { fg = '#C3E88D' })
+local M = {}
+
+local function apply_diagnostic_highlights()
+  local highlights = {
+    DiagnosticVirtualTextError = '#FF5370',
+    DiagnosticVirtualTextWarn = '#FFCB6B',
+    DiagnosticVirtualTextInfo = '#82AAFF',
+    DiagnosticVirtualTextHint = '#C3E88D',
+  }
+
+  for group, color in pairs(highlights) do
+    vim.api.nvim_set_hl(0, group, { fg = color })
+  end
+end
+
+M.apply = apply_diagnostic_highlights
+
+apply_diagnostic_highlights()
+
+vim.api.nvim_create_autocmd('ColorScheme', {
+  callback = apply_diagnostic_highlights,
+})
+
+return M
