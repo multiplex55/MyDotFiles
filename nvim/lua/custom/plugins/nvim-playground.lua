8511072227fedb603d55eb1209ec0caa456dce23
multiplex55 <6619098+multiplex55@users.noreply.github.com>
2026-04-30 18:07:21 -0400

    Adjust markdown autocmd ordering and disable playground plugin

--- nvim/lua/custom/plugins/nvim-playground.lua
+++ nvim/lua/custom/plugins/nvim-playground.lua
@@ -1,16 +1,6 @@
 return {
-
   {
     'nvim-treesitter/playground',
-    cmd = 'TSPlaygroundToggle',
-    config = function()
-      require('nvim-treesitter.configs').setup {
-        playground = {
-          enable = true,
-          updatetime = 25,
-          persist_queries = false,
-        },
-      }
-    end,
+    enabled = false,
   },
 }
