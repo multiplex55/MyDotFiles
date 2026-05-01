3ead52e6ff2824326522c3f51f6ef66b81e01650
multiplex55 <multiplex55@yahoo.com>
2026-05-01 19:03:54 -0400

    Update md-render.lua

--- nvim/lua/custom/plugins/md-render.lua
+++ nvim/lua/custom/plugins/md-render.lua
@@ -11,7 +11,7 @@ return {
       { '<leader>mp', '<Plug>(md-render-preview)', mode = 'n', ft = { 'markdown' } },
       { '<leader>mt', '<Plug>(md-render-preview-tab)', mode = 'n', ft = { 'markdown' } },
       { '<leader>md', '<Plug>(md-render-demo)', mode = 'n' },
-      { 'sm', '<cmd>Telescope md_render find_files<cr>', mode = 'n', ft = { 'markdown' }, desc = '[S]earch [M]arkdown' },
+      { '<leader>sm', '<cmd>Telescope md_render find_files<cr>', mode = 'n', ft = { 'markdown' }, desc = '[S]earch [M]arkdown' },
     },
   },
 }
